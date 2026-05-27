773ac91761f3f1de2217301979d241e521ce9587
jaisan <jaisanaj1999@gmail.com>
2026-05-27 18:05:50 +0530

    changed send email function network address

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,17 +1,28 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport/index.js";
 
 export const sendEmail = async (
   to: string,
   subject: string,
   text: string,
 ): Promise<void> => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
+  const mailOptions: SMTPTransport.Options = {
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
-  });
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+
+    ...({ family: 4 } as any),
+  };
+
+  const transporter = nodemailer.createTransport(mailOptions);
 
   await transporter.sendMail({
     from: `"The Reserve" <${process.env.EMAIL_USER}>`,
